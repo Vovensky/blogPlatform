4f69b4a8da22f2d17adfdb070e4400cafc387232
Vladimir <urinskii1996@gmail.com>
2024-07-14 22:23:46 +0500

    configurated eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,40 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import reactHooks from 'eslint-plugin-react-hooks'
+import configPrettier from 'eslint-config-prettier'
+import reactReduxPlugin from 'eslint-plugin-react-redux'
+import reactRefreshPlugin from 'eslint-plugin-react-refresh'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  { languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2023,
-      }, 
-      parserOptions: {
-        ecmaFeatures: { 
-          jsx: true 
-        }, 
-      } 
-    } 
-  },
-  { languageOptions: { 
-      globals: globals.browser, 
-    }
-  },
-  pluginJs.configs.recommended,
-  pluginReactConfig,
+    {
+        name: 'test1',
+        ignores: ['node_modules', 'dist'],
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-redux': reactReduxPlugin,
+            'react-refresh': reactRefreshPlugin,
+            react: pluginReactConfig,
+        },
+        languageOptions: {
+            globals: {
+                ...globals.serviceworker,
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2023,
+            },
+            parserOptions: {
+                ...pluginReactConfig.configs.recommended.parserOptions,
+                jsx: true,
+            },
+        },
+        files: ['**/*.{js,mjs,cjs,jsx}'],
+        rules: {
+            ...configPrettier.rules,
+            ...pluginJs.configs.recommended.rules,
+            ...pluginReactConfig.configs.recommended.rules,
+        },
+    },
+    eslintPluginPrettier,
 ]
